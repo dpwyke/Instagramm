b3c9ecf030e244e633a3b8e822fe7b1b469b72e9
glehr058 <dpwyke@gmail.com>
2018-12-06 14:29:22 -0500

    Added styling and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :pics do
+  	member do
+  		put "like", to: "pics#upvote"
+  	end
+  end
+  
+  root "pics#index"
 end
